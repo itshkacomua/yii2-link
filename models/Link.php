b1fc3d258a6c91e06c943c3eb0b03392f9e58c98
Andrey <progandrey@gmail.com>
2019-12-08 00:04:55 +0200

    create Models Link

---
+++ models/Link.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace itshkacomua\link\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "link".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $url
+ * @property int|null $parent_id
+ * @property int $created_at
+ * @property int $user_id
+ */
+class Link extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'link';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'url', 'created_at', 'user_id'], 'required'],
+            [['parent_id', 'created_at', 'user_id'], 'integer'],
+            [['name'], 'string', 'max' => 100],
+            [['url'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'url' => Yii::t('app', 'Url'),
+            'parent_id' => Yii::t('app', 'Parent ID'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'user_id' => Yii::t('app', 'User ID'),
+        ];
+    }
+}
